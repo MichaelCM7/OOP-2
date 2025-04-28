184c3d1b016c9c4261f2c9b584946a68ccd92ef0
Michael Mbugua <michael.mbugua@strathmore.edu>
2025-04-28 16:24:13 +0300

    ignore .idea

--- src/OOP2_CODING_CHALLENGES/CodingChallenge_5/main.java
+++ src/OOP2_CODING_CHALLENGES/CodingChallenge_5/main.java
@@ -1,4 +1,8 @@
 package OOP2_CODING_CHALLENGES.CodingChallenge_5;
 
 public class main {
+    public static void main(String[] args){
+        HelloTillStop greetings = new HelloTillStop();
+        greetings.showHello();
+    }
 }
