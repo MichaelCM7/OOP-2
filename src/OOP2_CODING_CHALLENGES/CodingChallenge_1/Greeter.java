2c68576f80077fdb00b6a1cefe6b5ff9379aed98
Michael Mbugua <michael.mbugua@strathmore.edu>
2025-04-29 08:24:16 +0300

    added done ex 1 and 2

--- src/OOP2_CODING_CHALLENGES/CodingChallenge_1/Greeter.java
+++ src/OOP2_CODING_CHALLENGES/CodingChallenge_1/Greeter.java
@@ -1,4 +1,12 @@
 package OOP2_CODING_CHALLENGES.CodingChallenge_1;
 
 public class Greeter {
+
+    void greet(){
+        System.out.println("Hi there");
+    }
+
+    public static void main(String[] args) {
+        (new Greeter()).greet();
+    }
 }
---
+++ src/OOP2_CODING_CHALLENGES/CodingChallenge_3/GradingSystem.java
@@ -0,0 +1,4 @@
+package OOP2_CODING_CHALLENGES.CodingChallenge_3;
+
+public class GradingSystem {
+}
