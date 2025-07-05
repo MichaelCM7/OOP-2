dd474b8729fa644d9374117771fc7af8512127b5
Michael Mbugua <michael.mbugua@strathmore.edu>
2025-07-05 10:17:12 +0300

    add final coding challenges

---
+++ CodingChallenges12_Exceptions/ArithmeticJava.java
@@ -0,0 +1,11 @@
+package OOP2_Coding_Challenges.CodingChallenge_12_Exceptions;
+
+public class ArithmeticJava {
+    public static void main(String[] args){
+        int firstNo = 100;
+        int secondNo = 0;
+        int answer;
+        answer = firstNo / secondNo;
+        System.out.println("Answer: " + answer);
+    }
+}
---
+++ CodingChallenges13_TryCatchFinally/ArithmeticJava.java
@@ -0,0 +1,17 @@
+package OOP2_Coding_Challenges.CodingChallenge_13_TryCatchFinally;
+
+public class ArithmeticJava {
+    public static void main(String[] args){
+        int firstNo = 100;
+        int secondNo = 0;
+        int answer;
+        try{
+            answer = firstNo / secondNo;
+            System.out.println("Answer: " + answer);
+        } catch(Exception e){
+            System.out.println("Error: Division by zero not allowed");
+        } finally {
+            System.out.println("The program has continued");
+        }
+    }
+}
