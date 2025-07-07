cbc0de4a4df134c83deabb66ac4ca90915dce4d6
Michael Mbugua <michael.mbugua@strathmore.edu>
2025-07-07 16:52:34 +0300

    finish coding challenges 15 and 16

---
+++ CodingChallenges15_RecursionSum/RecursiveSum.java
@@ -0,0 +1,16 @@
+package OOP2_Coding_Challenges.CodingChallenges15_RecursionSum;
+
+public class RecursiveSum {
+    public static int recSum(int num){
+        if(num > 1){
+            return num + recSum(num-1);
+        } else {
+            return 1;
+        }
+    }
+
+    public static void main(String[] args){
+        int num = 5;
+        System.out.println("Sum: " + recSum(num));
+    }
+}
---
+++ CodingChallenges16_Factorial/Factorial.java
@@ -0,0 +1,16 @@
+package OOP2_Coding_Challenges.CodingChallenges16_Factorial;
+
+public class Factorial {
+    public static int factorial(int num){
+        if(num > 1){
+            return num * factorial(num-1);
+        } else {
+            return 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        int num = 5;
+        System.out.println("Factorial: " + factorial(num));
+    }
+}
